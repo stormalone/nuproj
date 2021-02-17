a2726b4848821e4680032b0ec0868b6414525eda
Michael Angerman <stormasm@gmail.com>
2021-02-17 09:49:57 -0800

    mw

--- parseval/src/main.rs
+++ parseval/src/main.rs
@@ -1,7 +1,7 @@
-//use cli::{run_nu};
+use parseval::cli::run_nu;
 
 pub async fn check() {
-    let result = parseval::cli::run_nu("echo 'hello'".to_string()).await;
+    let result = run_nu("echo 'hello'".to_string()).await;
     println!("{:#?}", result);
 }
 
